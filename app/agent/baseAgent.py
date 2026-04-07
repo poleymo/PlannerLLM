f7c70496dc2d58ce6c994efd7cf3faebf0dddb8a
K-KY <kyuyoungk@naver.com>
2026-04-07 21:37:18 +0900

    feat: agent 공통 클래스
    - 모든 agent는 baseAgent로부터 상속받아 사용

---
+++ app/agent/baseAgent.py
@@ -0,0 +1,9 @@
+from app.infra.llm.client import LLMClient
+
+
+class BaseAgent:
+    def __init__(self, llm_client:LLMClient):
+        self.llm = llm_client
+
+    def run(self, *args, **kwargs):
+        raise NotImplementedError
