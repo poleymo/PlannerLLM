e0c23683438f4322ef92248a72e731a5082a1505
hjn5018 <hjn5018@gmail.com>
2026-04-17 17:20:14 +0900

    get_agent_service() -> get_intent_service()
    현재 Agent는 IntentParser뿐이다.
    
    기획서 상 Agent에는 Planner, Executor, Verifier, Response등 4가지가
    있다.
    하지만 현재 IntentParser가 Planner의 역할을 하고 있으며, Executor는
    Front에서 playwright을 통해 이루어지고 있다.
    Verifier는 playwright의 실행 동작 자체를 playwright가 검수하거나, 동작
    후의 html 코드 또는 스크린 샷 등을 Backend에서 넘겨 받아 처리하는 방법이
    있겠다. 우선은 playwright을 통한 검수를 통해 처리될 것을 기원한다.
    Response는 Front에서 playwright 동작을 수행한 후 message를 반환하는
    형식으로 되어있다.
    
    이후 앞서 기술한 Agent들을 구현한다면 get_executor_service()등과 같이
    새로 추가하는 것이 좋을 것 같다.

--- app/api/routes/agent.py
+++ app/api/routes/agent.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.api.dependent_manager import get_agent_service
+from app.api.dependent_manager import get_intent_service
 from app.service.agentService import AgentService
 from pydantic import BaseModel
 
@@ -15,9 +15,9 @@ class PlanPromptRequest(BaseModel):
 
 
 @router.post("/plan")
-def plan(req: PlanRequest, service: AgentService = Depends(get_agent_service)):
+def plan(req: PlanRequest, service: AgentService = Depends(get_intent_service)):
     return service.generate_plan(req.instruction)
 
 @router.post("/plan-prompt")
-def plan_prompt(req: PlanPromptRequest, service: AgentService = Depends(get_agent_service)):
+def plan_prompt(req: PlanPromptRequest, service: AgentService = Depends(get_intent_service)):
     return service.generate_plan_prompt(req.instruction, req.prompt)
